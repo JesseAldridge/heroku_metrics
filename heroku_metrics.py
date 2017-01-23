1d27edaa9f6d8cbe5cf3cec2c94da47d20594b86
Jesse Aldridge <JesseAldridge@gmail.com>
2017-01-23 11:28:11 -0800

    add sleep

--- heroku_metrics.py
+++ heroku_metrics.py
@@ -1,4 +1,4 @@
-import os
+import os, time
 
 import conf
 
@@ -7,4 +7,6 @@ url_templ = 'https://dashboard.heroku.com/apps/{}/metrics/{}?starting=24-hours-a
 
 for app_name in conf.app_names:
   for worker_name in conf.worker_names:
-    os.system('open {}'.format(url_templ.format(app_name, worker_name)))
+    url = url_templ.format(app_name, worker_name)
+    os.system('open {}'.format(url))
+    time.sleep(.1)
